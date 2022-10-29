5b8b08094a50b67ff9f2d5789afceea76300329a
“dhanon” <dhanon68@gmail.com>
2022-10-29 12:59:55 +0600

    first commit

---
+++ Swap two numbers.playground/Contents.swift
@@ -0,0 +1,19 @@
+//import Foundation
+//
+//var Number1 = 32
+//var Number2 = 54
+//
+//print("Number 1 before swapping:", Number1)
+//print("Number 2 before swapping:", Number2)
+//
+//(Number1, Number2) = (Number2, Number1)
+//
+//print("\nNumber 1 after swapping:", Number1)
+//print("Number 2 after swapping:", Number2)
+
+// using Dictionary
+//var names = Dictonary<String,Int> ()
+var names = [ "Ajoy": 1, "Mohit": 2] 
+print(names)
+// it prints [Ajay: 1, Mohit: 2]
+
